661a08ffb47df7bb825fe4bbc36423e0218002c1
PranavBhoite <pranavbhoite02@gmail.com>
2025-06-05 15:18:28 +0530

    Homepage added

--- registration-model/src/App.js
+++ registration-model/src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registration from './components/Authentication/Volunteer/Registration';
 import Login from './components/Authentication/Volunteer/Login';
 import Dashboard from './components/Authentication/Volunteer/Dashboard';
+import TMGFHomepage from './components/Home/TMGFHomepage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path = "/" element = {<Registration/>}/>
+        <Route path = "/" element = {<TMGFHomepage/>}/>
         <Route path="/login" element={<Login/>} />
+        <Route path="/Registration" element={<Registration/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
 
       </Routes>
