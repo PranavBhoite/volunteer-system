8648882b3ee99fca6256b8ae64782a9e43d529ac
KulkarniShripad <shrikulkarni2205@gmail.com>
2025-06-15 22:03:39 +0530

    Old dashboard changes

--- registration-model/src/App.js
+++ registration-model/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Registration from './components/Authentication/Volunteer/Registration';
-import Login from './components/Authentication/Volunteer/Login';
-import Dashboard from './components/Authentication/Volunteer/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import TMGFHomepage from './components/Home/TMGFHomepage';
 import VolunteerView from './components/Event/VolunteerView';
 import OrganizerView from './components/Event/OrganizerView';
@@ -11,6 +10,9 @@ import AdminLogin from './components/Authentication/Admin/AdminLogin';
 import AdminDashboard from './components/Authentication/Admin/AdminDashboard';
 import HelpSection from './components/Help/HelpSection';
 import AllVolunteers from './components/Features/AdminDashboard/AllVolunteers/AllVolunteers';
+import Login from './components/Authentication/Volunteer/Login';
+import Registration from './components/Authentication/Volunteer/Registration';
+import DashboardLayout from './components/Authentication/Volunteer/Dashboard/layouts/DashboardLayout';
 
 function App() {
   return (
@@ -19,7 +21,7 @@ function App() {
         <Route path = "/" element = {<TMGFHomepage/>}/>
         <Route path="/login" element={<Login/>} />
         <Route path="/Registration" element={<Registration/>} />
-        <Route path="/dashboard/:userId" element={<Dashboard />} />
+        {/* <Route path="/dashboard/:userId" element={<Dashboard />} /> */}
         <Route path="/volunteer-view/:userId" element={<VolunteerView/>} />
         <Route path="/organizer-view" element={<OrganizerView/>} />
          <Route path="/admin/register" element={<AdminRegister />} />
@@ -27,6 +29,10 @@ function App() {
          <Route path="/admin-dashboard/:uid" element={<AdminDashboard />} />
         <Route path="/help-section/:uid" element={<HelpSection />} />
         <Route path="/allvolunteers" element={<AllVolunteers />} />
+        <Route path="/" element={<TMGFHomepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/*:userId" element={<DashboardLayout />} />
       </Routes>
     </Router>
   );
---
+++ registration-model/src/components/Features/VolunteerDashboard/pages/HelpDisapproved.jsx
@@ -0,0 +1,3 @@
+export default function HelpDisapproved() {
+  return null;
+}
